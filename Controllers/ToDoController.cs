f15755db72dad5d828e8005548bbaffe46415687
rory <roryjavant@gmail.com>
2019-07-25 09:44:27 -0500

    Added ToDo_Context, ToDoController, ToDoItemModel

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoController(ToDoContext context)
+        {
+            _context = context;
+
+            if (_context.ToDoItems.Count() == 0)
+            {
+                // Create a new ToDoItem if collection is empyt,
+                // which means you can't delete all ToDoItems.
+                _context.ToDoItems.Add(new ToDoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/ToDo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDoItem>>> GetToDoItem()
+        {
+            return await _context.ToDoItems.ToListAsync();
+        }
+
+        // GET: api/ToDo/5
+        [HttpGet("id")]
+        public async Task<ActionResult<ToDoItem>> GetToDoItem(long id)
+        {
+            var toDoItem = await _context.ToDoItems.FindAsync(id);
+
+            if (toDoItem == null)
+            {
+                return NotFound();
+            }
+            return toDoItem;
+        }
+
+        // POST: api/ToDo
+        [HttpPost]
+        public async Task<ActionResult<ToDoItem>> PostToDoItem(ToDoItem item)
+        {
+            _context.ToDoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetToDoItem), new { id = item.id }, item); ;
+
+        }
+    }
+}
